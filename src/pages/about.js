582b124142cdfde8ada66f473ca12a0b2166e6e3
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-04-04 11:13:53 +0600

    portfolio website nafisbd.com

--- src/pages/about.js
+++ src/pages/about.js
@@ -49,7 +49,7 @@ const about = () => {
         </Head>
         <main className='flex flex-col w-full items-center justify-center'>
             <Layout className="pt-16">
-                <AnimatedText text="Passion Fuels Purpose!" className='mb-16' />
+                <AnimatedText text="Passion Fuels Purpose!" className='mb-16 ' />
                 <div className='grid w-full grid-cols-8 gap-16'>
                     <div className='col-span-3 flex flex-col items-start justify-start'>
                         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>About Me</h2>
