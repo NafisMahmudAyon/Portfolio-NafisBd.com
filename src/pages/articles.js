3e28adc83e703085c5843fa78d48585ddaa298ea
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-04-04 10:58:55 +0600

    portfolio nafisbd.com

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,13 +3,7 @@ import React from 'react'
 const articles = () => {
   return (
     <>
-        <Head>
-            <title>NafisBD | About Me</title>
-            <meta name="description" content="NafisBd.com is a protfolio website which created by Nafis Mahmud Ayon" />
-        </Head>
-        <main>
-            <AnimatedText />
-        </main>
+        Hello
     </>
   )
 }
