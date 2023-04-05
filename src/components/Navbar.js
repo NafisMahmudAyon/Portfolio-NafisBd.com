2407bb6ac41cbfbfe645533ffcacbb6f891e697c
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-04-05 21:03:35 +0600

    starting theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const CustomLink = ({ href, title, className=""}) => {
             {title}
 
             <span className={`h-[2px] inline-block group-hover:w-full transition-[width] ease duration-300 bg-dark absolute left-0 -bottom-0.5 
-            ${router.asPath === href ? 'w-full' : 'w-0'}
+            ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-light
             `}>&nbsp;</span>
         </Link>
     )
@@ -24,7 +24,7 @@ const Navbar = () => {
 
     const [mode, setMode] = useThemeSwitcher();
   return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'>
         <nav>
             <CustomLink href='/' title="Home" className=' uppercase font-bold mr-4' />
             <CustomLink href='/about' title="About" className=' uppercase font-bold mx-4' />
@@ -41,7 +41,7 @@ const Navbar = () => {
             <motion.a href="mailto:nafismahmudayon@gmail.com" target={"_blank"} whileHover={{ y:-2 }} whileTap={{ scale:0.9 }} className='w-6 ml-3'><GmailIcon /></motion.a>
             <button
                 onClick={() => setMode(mode === "light" ? "dark" : "light")}
-                className='ml-3 flex items-center justify-center rounded-full p-1'
+                className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
             >
                 {
                     mode === "dark" ?
