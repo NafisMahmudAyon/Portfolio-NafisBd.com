cd653271227a28d9e1f5b247f8e072987142ff64
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-04-05 15:16:53 +0600

    portfolio website nafisbd.com working on article page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,13 +6,19 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { GithubIcon } from '@/components/Icons'
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+import { motion } from 'framer-motion'
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
     <article className='w-full relative flex items-center justify-between rounded-3xl rounded-br-2xl border border-solid border-dark bg-light shadow-2xl p-12'>
       <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark" />
       <Link href={link} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
-        <Image src={img} alt={title} className='w-full h-auto' />
+        <FramerImage src={img} alt={title} className='w-full h-auto' 
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
         <span className='text-primary font-medium text-xl'>{type}</span>
@@ -36,7 +42,10 @@ const Project = ({ type, title, img, link, github }) => {
     <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative'>
       <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark" />
       <Link href={link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg'>
-        <Image src={img} alt={title} className='w-full h-auto' />
+        <FramerImage src={img} alt={title} className='w-full h-auto' 
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className='w-full flex flex-col items-start justify-between mt-4'>
         <span className='text-primary font-medium text-xl'>{type}</span>
