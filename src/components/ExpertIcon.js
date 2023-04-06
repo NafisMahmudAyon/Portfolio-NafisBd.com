0ca7e734d1d8436db355b6d48701ff99d9fc2017
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-04-06 22:10:41 +0600

    full website complete

--- src/components/ExpertIcon.js
+++ src/components/ExpertIcon.js
@@ -30,19 +30,19 @@ const ExpertIcon = () => {
   return (
     <div className='flex items-center justify-center flex-wrap'>
             
-            <Image src={html} alt="HTML" title={title.props='HTML'} className='dot w-[28px] mr-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={css} alt="CSS" title={title.props='CSS'} className='dot w-[28px] mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={js} alt="JavaScript" title={title.props='JavaScript'} className='dot w-[28px] mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={react} alt="ReactJS" title={title.props='ReactJS'} className='dot w-[28px] mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={wordpress} alt="WordPress" title={title.props='WordPress'} className='dot w-[28px] mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={sass} alt="Sass" title={title.props='Sass'} className='dot w-[28px] mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={tailwind} alt="Tailwind" title={title.props='Tailwind'} className='dot w-[28px] mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={bootstrap} alt="Bootstrap" title={title.props='Bootstrap'} className='dot w-[28px] mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={php} alt="PHP" title={title.props='PHP'} className='dot w-[28px] mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={mysql} alt="MySQL" title={title.props='MySQL'} className='dot w-[28px] mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={illustrator} alt="Illustrator" title={title.props='Illustrator'} className='dot w-[28px] mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={photoshop} alt="Photoshop" title={title.props='Photoshop'} className='dot w-[28px] mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
-            <Image src={figma} alt="Figma" title={title.props='Figma'} className='dot w-[28px] ml-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={html} alt="HTML" title={title.props='HTML'} className='dot w-[28px] mt-2 mr-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={css} alt="CSS" title={title.props='CSS'} className='dot w-[28px] mt-2 mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={js} alt="JavaScript" title={title.props='JavaScript'} className='dot w-[28px] mt-2 mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={react} alt="ReactJS" title={title.props='ReactJS'} className='dot w-[28px] mt-2 mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={wordpress} alt="WordPress" title={title.props='WordPress'} className='dot w-[28px] mt-2 mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={sass} alt="Sass" title={title.props='Sass'} className='dot w-[28px] mt-2 mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={tailwind} alt="Tailwind" title={title.props='Tailwind'} className='dot w-[28px] mt-2 mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={bootstrap} alt="Bootstrap" title={title.props='Bootstrap'} className='dot w-[28px] mt-2 mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={php} alt="PHP" title={title.props='PHP'} className='dot w-[28px] mt-2 mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={mysql} alt="MySQL" title={title.props='MySQL'} className='dot w-[28px] mt-2 mx-[8px] h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={illustrator} alt="Illustrator" title={title.props='Illustrator'} className='dot w-[28px] mt-2 mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={photoshop} alt="Photoshop" title={title.props='Photoshop'} className='dot w-[28px] mt-2 mx-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
+            <Image src={figma} alt="Figma" title={title.props='Figma'} className='dot w-[28px] mt-2 ml-[8px]  h-auto transition ease-in-out  hover:-translate-y-2 hover:scale-150' />
         
     </div>
   )
